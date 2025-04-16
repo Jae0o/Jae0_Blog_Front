1383fe753ec77e195d3de2ae12a8cf3cdee82434
Lee Jae Yeong <leey153@naver.com>
2025-04-16 15:28:49 +0900

    ✏️ Feat :  PostLayout 구현

---
+++ src/app/(WithMenubar)/post/layout.tsx
@@ -0,0 +1,23 @@
+import { PropsWithChildren } from "react";
+
+import { TopDecorator } from "./_components";
+
+import { twMerge } from "tailwind-merge";
+
+const PostLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <section
+      className={twMerge(
+        "w-full h-full px-[4rem] py-[1rem] flex items-center justify-center",
+        "tablet:p-[1rem]",
+      )}
+    >
+      <article className="w-full max-w-[120rem] h-full flex flex-col border-2 rounded-[1.2rem] shadow-medium">
+        <TopDecorator />
+        {children}
+      </article>
+    </section>
+  );
+};
+
+export default PostLayout;
