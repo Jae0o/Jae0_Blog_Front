433f015892d634719746fe3c95825bce2ef1a382
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-04-26 22:43:13 +0900

    ✏️ Feat : FRONTEND_BANNER_IMAGE 추가

--- src/app/(WithMenubar)/post/list/[category]/_assets/index.ts
+++ src/app/(WithMenubar)/post/list/[category]/_assets/index.ts
@@ -1,3 +1,4 @@
 export { default as BLOG_BANNER_IMAGE } from "./BLOG_BANNER_IMAGE.png";
 export { default as MASILGASIL_BANNER_IMAGE } from "./MASILGASIL_BANNER_IMAGE.png";
 export { default as YAP_BANNER_IMAGE } from "./YAP_BANNER_IMAGE.png";
+export { default as FRONTEND_BANNER_IMAGE } from "./FRONTEND_BANNER_IMAGE.png";
