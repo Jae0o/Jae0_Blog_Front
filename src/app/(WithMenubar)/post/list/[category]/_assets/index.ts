7523541d0c1735b399dc25dc91612ebce11e02c0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-04-26 22:50:23 +0900

    ✏️ Feat : DIARY_BANNER_IMAGE 추가

--- src/app/(WithMenubar)/post/list/[category]/_assets/index.ts
+++ src/app/(WithMenubar)/post/list/[category]/_assets/index.ts
@@ -2,3 +2,4 @@ export { default as BLOG_BANNER_IMAGE } from "./BLOG_BANNER_IMAGE.png";
 export { default as MASILGASIL_BANNER_IMAGE } from "./MASILGASIL_BANNER_IMAGE.png";
 export { default as YAP_BANNER_IMAGE } from "./YAP_BANNER_IMAGE.png";
 export { default as FRONTEND_BANNER_IMAGE } from "./FRONTEND_BANNER_IMAGE.png";
+export { default as DIARY_BANNER_IMAGE } from "./DIARY_BANNER_IMAGE.png";
