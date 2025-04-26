d7d0e996a288ead6eb6d10060759e16908c4f8de
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-04-26 22:42:17 +0900

    ✏️ Feat : YAP_BANNER_IMAGE 추가

--- src/app/(WithMenubar)/post/list/[category]/_assets/index.ts
+++ src/app/(WithMenubar)/post/list/[category]/_assets/index.ts
@@ -1,2 +1,3 @@
 export { default as BLOG_BANNER_IMAGE } from "./BLOG_BANNER_IMAGE.png";
 export { default as MASILGASIL_BANNER_IMAGE } from "./MASILGASIL_BANNER_IMAGE.png";
+export { default as YAP_BANNER_IMAGE } from "./YAP_BANNER_IMAGE.png";
