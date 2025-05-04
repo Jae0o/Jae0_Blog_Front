e2ffac4893fbecab916d0c1717a1cba4b06d6abb
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-05-04 20:02:58 +0900

    ✏️ Feat : PostInfo 적용 및 Division 적용

--- src/app/(WithMenubar)/post/detail/[postId]/page.tsx
+++ src/app/(WithMenubar)/post/detail/[postId]/page.tsx
@@ -1,7 +1,9 @@
+import { Division } from "@/lib/components/server";
+
 import { getPost } from "@/lib/services";
 
 import { PostDetailPageProps } from "./PostDetailPage.type";
-import { PostDetailBanner } from "./_components";
+import { PostDetailBanner, PostInfo } from "./_components";
 
 const PostDetailPage = async ({ params }: PostDetailPageProps) => {
   const { postId } = await params;
@@ -9,11 +11,24 @@ const PostDetailPage = async ({ params }: PostDetailPageProps) => {
   const post = await getPost({ postId });
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full flex flex-col gap-[1rem]">
       <PostDetailBanner
         thumbnail={post.thumbnail}
         title={post.title}
       />
+
+      <div className="w-full px-[2rem] flex flex-col gap-[1rem]">
+        <Division />
+
+        <PostInfo
+          category={post.category}
+          tags={post.tag}
+          createAt={post.createAt}
+          updateAt={post.updateAt}
+        />
+
+        <Division />
+      </div>
     </div>
   );
 };
