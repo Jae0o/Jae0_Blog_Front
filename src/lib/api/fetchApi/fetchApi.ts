cb82566e539199abb4e3ad4c1ff2ac71ea940c3e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-05-09 23:10:55 +0900

    ✏️ Feat :  Custom FetchApi

--- src/lib/api/fetchApi/fetchApi.ts
+++ src/lib/api/fetchApi/fetchApi.ts
@@ -1,9 +1,54 @@
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+// const fetchApi = (url: string, options: RequestInit = {}) => {
+//   return fetch(`${API_KEY}${url}`, {
+//     ...options,
+//   });
+// };
 
-const fetchApi = (url: string, options: RequestInit = {}) => {
-  return fetch(`${API_KEY}${url}`, {
-    ...options,
-  });
-};
+// export default fetchApi;
 
+class FetchApi {
+  private static API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+  private static headerJson = {
+    "Content-Type": "application/json",
+  };
+
+  private static apiInstance = async <T>(
+    method: string,
+    url: string,
+    options: RequestInit = {},
+  ) => {
+    const res = await fetch(`${this.API_KEY}${url}`, {
+      method,
+      ...options,
+    });
+
+    if (!res) {
+      // TODO : Error Control
+      throw new Error(`API Error : ${url}`);
+    }
+
+    if (!res.ok) {
+      const { code } = await res.json();
+
+      // TODO : Error Control
+      console.log("Test : TEST , ", code);
+    }
+
+    const result: T = await res.json();
+    return result;
+  };
+
+  get = async <T>(url: string, options: RequestInit = {}) => {
+    return await FetchApi.apiInstance<T>("GET", url, {
+      headers: {
+        ...FetchApi.headerJson,
+        ...options.headers,
+      },
+      ...options,
+    });
+  };
+}
+
+const fetchApi = new FetchApi();
 export default fetchApi;
