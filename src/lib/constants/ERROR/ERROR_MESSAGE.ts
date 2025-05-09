5abe6974842eaafe7eac77433fea06972c33d0f4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-05-09 23:57:14 +0900

    🔨 Modify:  ERROR_MESSAGE 로직 수정

--- src/lib/constants/ERROR/ERROR_MESSAGE.ts
+++ src/lib/constants/ERROR/ERROR_MESSAGE.ts
@@ -1,12 +1,16 @@
 const DEFAULT_ERROR_MESSAGE = "불러오는 과정에서 문제가 발생했어요!";
 
+const INVALID_MESSAGE = "찾을 수 없는 정보였어요..";
+
 export const ERROR_MESSAGE = (code: number) => {
   switch (code) {
+    case 202002:
+      return INVALID_MESSAGE;
+    case 501002:
+      return INVALID_MESSAGE;
+
     case 201001 | 202001 | 203001 | 501001:
       return DEFAULT_ERROR_MESSAGE;
-
-    case 202002 | 501002:
-      return "찾을 수 없는 정보였어요..";
 
     default:
       return DEFAULT_ERROR_MESSAGE;
